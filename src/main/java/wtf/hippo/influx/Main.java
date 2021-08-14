63e17a44018c57e9b7cdfcf834f40b2e8c1d65c5
Pat <65928079+HippoBaguette@users.noreply.github.com>
2021-08-14 16:51:16 +0800

    Update Main.java

--- src/main/java/wtf/hippo/influx/Main.java
+++ src/main/java/wtf/hippo/influx/Main.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import wtf.hippo.influx.commands.Commands;
+import wtf.hippo.influx.utils.KeepAlive;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class Main extends ListenerAdapter {
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("TOKEN");
         assert token != null;
+        KeepAlive.run()
         JDA bot = JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .setActivity(Activity.watching("the chats go by"))
                 .setStatus(OnlineStatus.IDLE)
